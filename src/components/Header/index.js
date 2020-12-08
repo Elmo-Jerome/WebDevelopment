fdec245267b5c26865babb9b48c980c2643a5d77
cachuela saimon <cachuelasaimon@gmail.com>
2020-12-09 00:30:17 +0800

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+
+
+const Header = props => {
+
+    return (
+        <header className="header">
+
+        </header>
+    )
+
+}
+
+export default Header
