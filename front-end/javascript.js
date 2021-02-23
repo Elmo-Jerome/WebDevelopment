16da39a6d0a9a84b5dbe2f7f00be68f00c078e48
Elmo jerome <60439291+Elmo-Jerome@users.noreply.github.com>
2021-02-24 02:42:10 +0800

    Update javascript.js

--- front-end/javascript.js
+++ front-end/javascript.js
@@ -1,1 +1,23 @@
+// quantity js
 
+let addBtn = document.querySelector('#plus-btn');
+let subBtn = document.querySelector('#minus-btn');
+let qty = document.querySelector('#qty_input');
+
+addBtn.addEventListener('click', () =>{
+    if(qty.value >= 20){
+        qty.value = 20;
+    }
+    else{
+        qty.value  =parseInt(qty.value) + 1;
+    }
+});
+
+subBtn.addEventListener('click', () =>{
+   if(qty.value <= 1){
+       qty.value = 1;
+   }
+   else{
+       qty.value  =parseInt(qty.value) - 1;
+   }
+});
